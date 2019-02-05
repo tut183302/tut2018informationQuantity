c656e04151fae21972a1d4d36c361dfd6e93ce37
Toshiki Aoba <aobatact144@gmail.com>
2019-02-05 15:07:28 +0900

    Add test

--- s4/B183301/Hello.java
+++ s4/B183301/Hello.java
@@ -4,10 +4,6 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
 	    System.out.println("HelloWorld");
-=======
-        System.out.println("HelloWorld");
->>>>>>> master
     }
 }
